fbb3894c1c74cc3c70c39a6244cbb24b7bba7842
WebFrontliners <148970541+WebFrontliners@users.noreply.github.com>
2024-04-10 11:53:51 +0530

    rephrase code

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ async function checkWeather(city){
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
-    }else{
-
+    }
+    else
+    {
         var data = await response.json();
 
         document.querySelector(".city").innerHTML = data.name;
@@ -38,11 +39,9 @@ async function checkWeather(city){
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
-
     }
 }
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value); 
 });
-
